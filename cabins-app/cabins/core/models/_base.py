1ca2e42da1ea626b2c40100da10eb6d2a0f1a4d8
Wes Garlock <wesley.g.garlock@gmail.com>
2020-12-19 14:37:35 -0500

    Setup image serialization - setup auto image serialization to generate useable urls from wagtails rendition system

--- cabins-app/cabins/core/models/_base.py
+++ cabins-app/cabins/core/models/_base.py
@@ -1,6 +1,10 @@
+from django.conf import settings
 from django.db import models
 from django.utils.functional import cached_property
 from rest_framework import serializers
+
+from cabins.core import get_image_model_string
+from cabins.core.cache import get_cached_class
 
 
 class Orderable(models.Model):
@@ -19,11 +23,22 @@ class Image(models.Model):
 
 class SeriailizerMixin:
     @cached_property
-    def serializer_model(self):
+    def serializer_model(self, exclude_tup=()):
         class SelfSerializer(serializers.ModelSerializer):
+            def build_relational_field(self, field_name, relation_info):
+                field_class, field_kwargs = super().build_relational_field(field_name, relation_info)
+                if relation_info.related_model._meta.label == get_image_model_string():
+                    class ImageField(serializers.RelatedField):
+                        def to_representation(self, value):
+                            return get_cached_class(settings.CORE_IMAGE_RENDITION).representation(value)
+
+                    field_class = ImageField
+
+                return field_class, field_kwargs
+
             class Meta:
                 model = self.__class__
-                fields = "__all__"
+                exclude = exclude_tup
 
         return SelfSerializer
 
