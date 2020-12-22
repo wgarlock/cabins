330559890868cd62479bead2b59b3134591619d9
Wes Garlock <wesley.g.garlock@gmail.com>
2020-12-21 20:26:24 -0500

    Add listing features - Add Amentities tags - Add website - Add street address

--- cabins-app/cabins/page/models.py
+++ cabins-app/cabins/page/models.py
@@ -1,8 +1,11 @@
 from django.core.cache import cache
 from django.db import models
+from modelcluster.fields import ParentalKey
+from taggit.models import TaggedItemBase
 
 from cabins.core import get_image_model_string
 from cabins.core.models import SeriailizerMixin
+from cabins.page import get_page_string
 
 
 class AbstractBasePage(SeriailizerMixin):
@@ -39,3 +42,43 @@ class BasePage(AbstractBasePage, models.Model):
     og_image = models.ForeignKey(
         get_image_model_string(), null=True, on_delete=models.SET_NULL, related_name='+'
     )
+
+
+class OpenDates(TaggedItemBase):
+    content_object = ParentalKey(get_page_string(), on_delete=models.CASCADE, related_name='amenities_open_dates_tags')
+
+
+class General(TaggedItemBase):
+    content_object = ParentalKey(get_page_string(), on_delete=models.CASCADE, related_name='amenities_general_tags')
+
+
+class FoodDrink(TaggedItemBase):
+    content_object = ParentalKey(get_page_string(), on_delete=models.CASCADE, related_name='food_drink_tags')
+
+
+class PropertySize(TaggedItemBase):
+    content_object = ParentalKey(get_page_string(), on_delete=models.CASCADE, related_name='propertysize_tags')
+
+
+class Room(TaggedItemBase):
+    content_object = ParentalKey(get_page_string(), on_delete=models.CASCADE, related_name='room_tags')
+
+
+class NearbyCity(TaggedItemBase):
+    content_object = ParentalKey(get_page_string(), on_delete=models.CASCADE, related_name='nearbycity_tags')
+
+
+class Activities(TaggedItemBase):
+    content_object = ParentalKey(get_page_string(), on_delete=models.CASCADE, related_name='activities_tags')
+
+
+class NearByWater(TaggedItemBase):
+    content_object = ParentalKey(get_page_string(), on_delete=models.CASCADE, related_name='nearbywater_tags')
+
+
+class Services(TaggedItemBase):
+    content_object = ParentalKey(get_page_string(), on_delete=models.CASCADE, related_name='services_tags')
+
+
+class Suitability(TaggedItemBase):
+    content_object = ParentalKey(get_page_string(), on_delete=models.CASCADE, related_name='suitability_tags')
